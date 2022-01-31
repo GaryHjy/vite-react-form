d03797f9c940fa76204d594cfc4834ba0ff855b4
GaryHjy <782316959@qq.com>
2022-02-01 00:32:53 +0800

    feat: init vite project

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { Input, Button } from 'antd'
+// import Form from '@/components/Form'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Button type='primary'>按钮</Button>
+
+      {/* <Form>
+        <Form.Item>
+          <Input />
+        </Form.Item>
+      </Form> */}
+    </div>
+  )
+}
+
+export default App
---
+++ vite.config.ts
@@ -0,0 +1,33 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import vitePluginImp from 'vite-plugin-imp'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: "antd",
+          style: (name) => `antd/lib/${name}/style/index.less`,
+        },
+      ],
+    })
+  ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        // 支持内联 JavaScript
+        javascriptEnabled: true,
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './'), // 根路径
+      '@': path.resolve(__dirname, 'src') // src 路径
+    }
+  }
+})
