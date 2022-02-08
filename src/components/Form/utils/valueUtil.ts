8ebd0d8fcd865fba27be245327b6e04451331666
GaryHjy <782316959@qq.com>
2022-02-08 20:09:25 +0800

    feat: 新增valueUtil

---
+++ src/components/Form/utils/valueUtil.ts
@@ -0,0 +1,27 @@
+import type { EventArgs, StoreValue } from "../interface";
+
+
+/**
+ * @description 判断是否为对象
+ * @param obj 
+ * @returns boolean
+ */
+function isObject(obj: StoreValue) {
+  return typeof obj === 'object' && obj !== null && Object.getPrototypeOf(obj) === Object.prototype;
+}
+
+/**
+ * @description 获取默认的组件值
+ * @param valuePropName 获取value指定的key值
+ * @param args 事件源对象
+ * @returns value
+ */
+export function defaultGetValueFromEvent(valuePropName: string, ...args: EventArgs) {
+  const event = args[0];
+  if (event && event.target && typeof event.target === 'object' && valuePropName in event.target) {
+    return event.target[valuePropName];
+  }
+  return event;
+}
+
+
