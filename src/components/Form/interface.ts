0043dc5f5779a442827822b69a6018eba116a844
GaryHjy <782316959@qq.com>
2022-02-09 17:15:11 +0800

    feat: 新增interface

--- src/components/Form/interface.ts
+++ src/components/Form/interface.ts
@@ -1,9 +1,16 @@
+export type EventArgs = any[];
 
 export type StoreValue = any;
 export type Store = Record<string, StoreValue>;
 
 
-export interface FormInstance<Values = any> {}
+export interface FormInstance<Values = any> {
+
+}
+
+export type InternalFormInstance = FormInstance & {
+  validateTrigger?: string | string[] | false;
+}
 
 
 export interface Callbacks<Values = any> {
