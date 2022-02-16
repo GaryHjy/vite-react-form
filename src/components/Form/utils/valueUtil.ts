fee72292edc47e852566c0162006256adb7a7f2e
GaryHjy <782316959@qq.com>
2022-02-17 05:11:23 +0800

    feat: 新增valueUtils

--- src/components/Form/utils/valueUtil.ts
+++ src/components/Form/utils/valueUtil.ts
@@ -24,4 +24,33 @@ export function defaultGetValueFromEvent(valuePropName: string, ...args: EventAr
   return event;
 }
 
+/**
+ * Copy values into store and return a new values object
+ * ({ a: 1, b: { c: 2 } }, { a: 4, b: { d: 5 } }) => { a: 4, b: { c: 2, d: 5 } }
+ */
+function internalSetValues<T>(store: any, values: any): T {
+  const newStore: any = (Array.isArray(store) ? [...store] : { ...store }) as T;
+
+  if (!values) {
+    return newStore;
+  }
+
+  Object.keys(values).forEach(key => {
+    const prevValue = newStore[key];
+    const value = values[key];
+
+    const recursive = isObject(prevValue) && isObject(value);
+    newStore[key] = recursive ? internalSetValues(prevValue, value || {}) : value;
+  });
+
+  return newStore;
+}
+
+
+export function setValues<T>(store: T, ...restValues: T[]): T {
+  return restValues.reduce(
+    (current: T, newStore: T): T => internalSetValues<T>(current, newStore),
+    store,
+  );
+}
 
