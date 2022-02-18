3e3143fa662c56c0157f9bd028f60087b0d8c7fb
GaryHjy <782316959@qq.com>
2022-02-18 15:45:12 +0800

    feat: 修改FormContext

--- src/components/Form/context/FormContext.tsx
+++ src/components/Form/context/FormContext.tsx
@@ -1,6 +1,22 @@
 import { createContext } from "react";
+import type { InternalFormInstance } from "../interface";
 
+const warningFunc: any = () => {};
 
-const FormContext = createContext({})
+const FormContext = createContext<InternalFormInstance>({
+  getFieldValue: warningFunc,
+  resetFields: warningFunc,
+  submit: warningFunc,
+
+  getInternalHooks: () => {
+
+    return {
+      dispatch: warningFunc,
+      registerField: warningFunc,
+      setInitialValues: warningFunc,
+      setCallbacks: warningFunc
+    }
+  }
+})
 
 export default FormContext
